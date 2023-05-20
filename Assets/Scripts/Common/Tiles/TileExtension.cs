b8ce4afba3718ed1bb8a8314c9651029cbffbe27
Danny <danny@192.168.1.5>
2023-05-20 18:04:27 +0700

    clean up + less triggers for workflows

--- Assets/Scripts/Common/Tiles/TileExtension.cs
+++ Assets/Scripts/Common/Tiles/TileExtension.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
-using static TileExtension;
 
 public class TileExtension : MonoBehaviour
 {

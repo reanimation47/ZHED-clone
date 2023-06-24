5328ec26a2f46c7b2e47e46321ebe6154a182ff4
Danny <danny@192.168.1.5>
2023-06-25 01:36:58 +0700

    finish locked packs logic

--- Assets/Scripts/StartMenu/LevelPacksScreen/Buttons/PackSelectButton.cs
+++ Assets/Scripts/StartMenu/LevelPacksScreen/Buttons/PackSelectButton.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using Core;
+
 namespace StartMenu.LevelPacksScreen.Buttons
 {
     public class PackSelectButton : BasicButtonClass
     {
         private int PackIndex;
         public TextMeshProUGUI title;
+        public Image img;
+        private Color LockedColor = new Color(145f / 255f, 151f / 255f, 174f / 255f);
 
-
+        private bool PackUnlocked = false;
         public void PackInit(int _index)
         {
             PackIndex = _index;
+            CheckProgress();
             SetTitle();
+        }
+
+        public void CheckProgress()
+        {
+            int CurrentUnlockedPack = PlayerPrefs.GetInt(CoreInfomation.PlayerPrefs_UnlockedPackIndex_Key);
+            Debug.Log(CurrentUnlockedPack);
+            if (PackIndex > CurrentUnlockedPack)
+            {
+                Debug.Log("Locked");
+                img.color = LockedColor;
+            }else
+            {
+                PackUnlocked = true;
+            }
         }
 
         private void SetTitle()
         {
+            if (!PackUnlocked)
+            {
+                title.text = "Locked";
+                return;
+            }
+
             if (PackIndex < 10)
             {
                 title.text = "Pack " + "0" +PackIndex;
@@ -25,10 +51,13 @@ namespace StartMenu.LevelPacksScreen.Buttons
             {
                 title.text = "Pack " + PackIndex;
             }
+
+            
         }
         
         public override void ButtonAction()
         {
+            if (!PackUnlocked) { return; }
             IStartMenu.LevelPackSelected(PackIndex);
         }
     }
